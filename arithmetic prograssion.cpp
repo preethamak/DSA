94005ca5894c6be09cccfed8b134c21296d370a3
preethamak <167765989+preethamak@users.noreply.github.com>
2024-06-23 23:56:43 +0530

    Add files via upload

---
+++ arithmetic prograssion.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, ap;
+    cout<<"enter the value of n";
+    cin>>n;
+    ap=3*n+7;
+    cout<<"nth term of the ap is"<<ap<<endl;
+    return 0;
+}
