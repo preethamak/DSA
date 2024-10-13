fb86d62d1d946f89254c82e3807ffa919414a2fc
preethamak <167765989+preethamak@users.noreply.github.com>
2024-10-13 17:59:34 +0530

    Add files via upload

---
+++ intToRoman.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ans = "";
+        int arr[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string s[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        for (int i = 0; i < 13; i++) {
+            while (num >= arr[i]) {
+                num -= arr[i];
+                ans += s[i];
+            }
+        }
+        
+        return ans;
+    }
