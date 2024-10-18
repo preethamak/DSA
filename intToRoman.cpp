0461816b85f44b3c6586147793028712d4a6f902
preethamak <167765989+preethamak@users.noreply.github.com>
2024-10-18 18:40:03 +0530

    Update intToRoman.cpp
    Comments

--- intToRoman.cpp
+++ intToRoman.cpp
@@ -7,10 +7,12 @@ class Solution {
 public:
     string intToRoman(int num) {
         string ans = "";
-        int arr[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int arr[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}; //storing the numbers in decresing order
         string s[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        //storing value of roman in decreasing values
         
-        for (int i = 0; i < 13; i++) {
+        for (int i = 0; i < 13; i++)
+            {
             while (num >= arr[i]) {
                 num -= arr[i];
                 ans += s[i];
