8a2acb0eb8ec1ed0d6cb922f63a6a7957d373740
preethamak <167765989+preethamak@users.noreply.github.com>
2024-10-22 07:18:00 +0530

    Add files via upload

---
+++ HappyNum.cpp
@@ -0,0 +1,33 @@
+#include <unordered_set>  // for unordered_set
+
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> st;
+
+        while (true) {
+            int val = 0;
+
+            while (n > 0) {
+                int temp = n % 10;
+                val = val + temp * temp;
+                n = n / 10;
+            }
+
+            if (val == 1) {
+                return true;
+            }
+
+            if (st.find(val) != st.end()) {
+                return false;
+            }
+
+            st.insert(val);
+            n = val;
+        }
+
+        return false;
+    }
+};
